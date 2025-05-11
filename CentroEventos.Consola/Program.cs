4ddb0ba4cf775ffdf09fe9723cd5f7dd5378911c
fortesinho <emilianoforte1999@hotmail.com>
2025-05-11 01:58:25 -0300

    testeo

--- CentroEventos.Consola/Program.cs
+++ CentroEventos.Consola/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("asdasddasd");
-Console.WriteLine("");
-Console.WriteLine("holaa");
+Console.WriteLine("que f");
+int i=10;
+Console.WriteLine(" la variable i vale: "+i);
