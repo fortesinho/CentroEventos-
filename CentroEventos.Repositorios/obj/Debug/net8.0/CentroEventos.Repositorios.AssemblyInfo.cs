999caa453ca6bacbe77fd6c70b06040085afcb03
fortesinho <emilianoforte1999@hotmail.com>
2025-06-13 18:42:02 -0300

    mensaje

--- CentroEventos.Repositorios/obj/Debug/net8.0/CentroEventos.Repositorios.AssemblyInfo.cs
+++ CentroEventos.Repositorios/obj/Debug/net8.0/CentroEventos.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caba5685f8cf5560e525cbd2a8c1e431499d1a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e3d769f337304d293c58aa00b41b715d7d6eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
