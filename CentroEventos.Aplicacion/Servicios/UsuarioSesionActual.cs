ba26d6d78cea68ba32ab7c233a693e6a30333f54
fortesinho <emilianoforte1999@hotmail.com>
2025-06-14 02:04:12 -0300

    mensaje

---
+++ CentroEventos.Aplicacion/Servicios/UsuarioSesionActual.cs
@@ -0,0 +1,20 @@
+
+
+using CentroEventos.Aplicacion.Entidades;
+namespace CentroEventos.Aplicacion.Servicios;
+public class UsuarioSesionActual
+{
+    public Usuario? Usuario { get; private set; }
+
+    public void IniciarSesion(Usuario usuario)
+    {
+        Usuario = usuario;
+    }
+
+    public void CerrarSesion()
+    {
+        Usuario = null;
+    }
+
+    public bool EstaAutenticado => Usuario is not null;
+}
